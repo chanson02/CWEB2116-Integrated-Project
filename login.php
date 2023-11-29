25d166a3d5d7d0b82aadb69ef957f91bdf74bae1
Cooper Hanson <cooperhanson2020@gmail.com>
2023-11-28 21:38:26 -0600

    Added admin column to users table

--- login.php
+++ login.php
@@ -207,7 +207,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
             // Validate credentials
             if (empty($username_err) && empty($password_err)) {
                 // Prepare a select statement
-                $sql = "SELECT id, username, password, fullname FROM users WHERE username = ?";
+                $sql = "SELECT id, username, password, fullname, admin FROM users WHERE username = ?";
 
                 if ($stmt = mysqli_prepare($db, $sql)) {
                     // Bind variables to the prepared statement as parameters
@@ -223,7 +223,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                         // Check if username exists, if yes then verify password
                         if (mysqli_stmt_num_rows($stmt) == 1) {
                             // Bind result variables
-                            mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password, $fullname);
+                            mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password, $fullname, $admin);
                             if (mysqli_stmt_fetch($stmt)) {
                                 if (password_verify($password, $hashed_password)) {
                                     // Password is correct, so start a new session
@@ -234,6 +234,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                                     $_SESSION["id"] = $id;
                                     $_SESSION["username"] = $username;
                                     $_SESSION["fullname"] = $fullname;
+                                    $_SESSION["admin"] = $admin;
                                     unset($_SESSION['error']);
                                     // Redirect user to welcome page
 
--- serverconnect.php
+++ serverconnect.php
@@ -12,6 +12,11 @@ $errors = array();
 //$db = mysqli_connect('remotemysql.com', 'aJNhE8Tihv', '9gY0DX3OdL', 'aJNhE8Tihv');
 $db = mysqli_connect('localhost', 'root', 'abc', 'EqManage');
 
+// Can change admin by doing
+// UPDATE users SET admin = 1 WHERE id=13;
+    # my user is 13, you can force me to get a notification by running
+    # INSERT INTO notification (target, message, status, datetime) VALUES (13, 'Notification!!!', 0, NOW());
+
 if ( mysqli_connect_errno() ) {
     // If there is an error with the connection, stop the script and display the error.
     die ('Failed to connect to MySQL: ' . mysqli_connect_error());
